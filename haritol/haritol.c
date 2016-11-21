ec02d1f3129c6c8fed13d48af941a6f3da0e1cf7
hikalium <hikalium@hikalium.com>
2016-11-21 22:39:15 +0900

    haritol: Add copy command

--- haritol/haritol.c
+++ haritol/haritol.c
@@ -10,8 +10,22 @@ int main(int argc, char *argv[])
 	if(argc < 2){
 		printf("%s remove files ...\n", argv[0]);
 		printf("%s concat outname srcfiles ...\n", argv[0]);
+		printf("%s copy from to\n", argv[0]);
 		return 0;
 	}
+
+	if(strcmp(argv[1], "copy") == 0){
+		char *tmp;
+		if(argc != 4){
+			printf("You should specify just 2 files.\n");
+			return 1;
+		}
+		argv[1] = "concat";
+		tmp = argv[2];
+		argv[2] = argv[3];
+		argv[3] = tmp;
+	}
+
 	if(strcmp(argv[1], "remove") == 0){
 		for(i = 2; i < argc; i++){
 			if(remove(argv[i])){
